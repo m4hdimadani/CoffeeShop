b3151eec2095e8b3e2848e27067683add9054cf2
MAHDIMADANI <m4hdim4d4ni@gmail.com>
2025-08-23 10:38:44 +0330

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,15 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {  Route, Routes } from "react-router-dom";
 
 import HomePage from "./components/HomePage";
 import Product from "./components/Product";
-import Index from "./components/[slug]";
 import Conformation from "./components/Conformation";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
-      <Route path="/product" element={<Product />}>
-        <Route path="/product/:id" element={<Index />} />
-      </Route>
-      <Route path="conformation" element={<Conformation />}/>
+      <Route path="/product" element={<Product />} />
+      <Route path="/conformation" element={<Conformation />} />
     </Routes>
   );
 }
